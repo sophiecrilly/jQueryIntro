7da36082100969aeafa439ebddb310a413f5f73e
sophiecrilly <crilly.sophie16@gmail.com>
2020-03-13 09:32:52 +1300

    introduction to jQuery

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// alert('javascript has loaded');
+
+// $
+// jQuery
+
+// $(selector).effect()
+
+$('#blueBox').hide();
+
+$('#greenBox').click(function(){
+    $('#blueBox').show();
+})
+
+$('#redBox').click(function(){
+    $('#blueBox').hide();
+})
