7bb74be42598661a0853ff0c11d1eb22fed63ee4
sophiecrilly <crilly.sophie16@gmail.com>
2020-03-16 10:07:30 +1300

    toggles for popups etc

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@
 // jQuery
 
 // $(selector).effect()
+            // .action
+            // .event
+
 
 $('#blueBox').hide();
 
@@ -13,4 +16,17 @@ $('#greenBox').click(function(){
 
 $('#redBox').click(function(){
     $('#blueBox').hide();
+})
+
+$('#modalExample1Button').click(function(){
+    // console.log('button has been clicked');
+    $('#modalExample1').removeClass('hidden').addClass('flex');
+    // $('#modalExample1').addClass('flex');
+    $('body').addClass('noScroll');
+});
+
+$('#modalExample1Close').click(function(){
+    // console.log('close button has been clicked');
+    $('#modalExample1').addClass('hidden').removeClass('flex');
+    $('body').removeClass('noScroll');
 })
